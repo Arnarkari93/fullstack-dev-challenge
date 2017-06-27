361c82b37b1adaa25f3081a5073b0d5d5dfe5f71
Arnar Kári Ágústsson <arnar@brimir.io>
2017-06-27 15:53:15 +0000

    remove state from component in favor of redux state

--- client/src/components/CurrencyInput.js
+++ client/src/components/CurrencyInput.js
@@ -1,44 +1,48 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import './CurrencyInput.css'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./CurrencyInput.css";
 
 export default class CurrencyInput extends Component {
-	constructor(props) {
-		super(props)
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			hasFocus: false,
-			value: props.defaultValue
-		}
-	}
+    this.state = {
+      hasFocus: false,
+      value: props.value
+    };
+  }
 
-	handleChange(e) {
-		const value = e.target.value
-		this.setState({value})
-	}
+  handleChange(e) {
+    const value = e.target.value;
+    this.props.onSet(+value);
+  }
 
-	handleFocus(e) {
-		this.setState({
-			hasFocus: true
-		})
-	}
+  handleFocus(e) {
+    this.setState({
+      hasFocus: true
+    });
+  }
 
-	render() {
-		const { defaultValue } = this.props
-		const { value } = this.state
+  render() {
+    const { value } = this.props;
 
-		return (
-			<div className={`currency-input ${defaultValue !== undefined ? 'default-value' : ''}`}>
-				<span>£</span>
-				<input type="text"
-					value={value}
-					onChange={this.handleChange.bind(this)}
-					onFocus={this.handleFocus.bind(this)}/>
-			</div>
-		)
-	}
+    return (
+      <div
+        className={`currency-input ${value !== undefined ? "default-value" : ""}`}
+      >
+        <span>£</span>
+        <input
+          type="text"
+          value={value}
+          onChange={this.handleChange.bind(this)}
+          onFocus={this.handleFocus.bind(this)}
+        />
+      </div>
+    );
+  }
 }
 
 CurrencyInput.propTypes = {
-	defaultValue: PropTypes.number
-}
+  value: PropTypes.number,
+  onSet: PropTypes.func
+};
