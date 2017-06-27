f281fd57dc7e783aa09d956e3cd8124df85ccf95
Arnar Kári Ágústsson <arnar@brimir.io>
2017-06-27 15:57:09 +0000

    adding packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,78 @@
-import React, { Component } from 'react';
-import CurrencyInput from './components/CurrencyInput'
-import SliderInput from './components/SliderInput'
-import DisplayGraph from './components/DisplayGraph'
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import CurrencyInput from "./components/CurrencyInput";
+import SliderInput from "./components/SliderInput";
+import DisplayGraph from "./components/DisplayGraph";
+import InterestCalculationType from "./components/InterestCalculationType";
+import ChangeCurrency from "./components/ChangeCurrency";
+import { fetchSavings, setField } from "./actions";
+import "./App.css";
 
 class App extends Component {
   render() {
+    const { savings, setField } = this.props;
+    const savingsData = !savings
+      ? []
+      : savings.map((s, i) => ({ month: i + 1, amount: s }));
+
     return (
       <div className="App">
         <div className="header-banner">
-          <h1 className="fmz-white-font">Finimize Interest Rate Calculator</h1>
+          <h1 className="fmz-white-font">
+            Finimize Interest Rate Calculator
+          </h1>
         </div>
-				<div className="financial-inputs">
-					<p className="input-label">How much have you saved?</p>
-					<CurrencyInput defaultValue={0}/>
+        <div className="financial-inputs">
+          <ChangeCurrency value="EUR" onSet={val => console.log("val", val)} />
+          <p className="input-label">How much have you saved?</p>
+          <CurrencyInput
+            value={this.props.initialSavings || 0}
+            onSet={value => setField({ initialSavings: value })}
+          />
 
-					<p className="input-label">How much will you save each month?</p>
-					<CurrencyInput defaultValue={0}/>
+          <p className="input-label">How much will you save each month?</p>
+          <CurrencyInput
+            value={this.props.monthlySavings || 0}
+            onSet={value => setField({ monthlySavings: value })}
+          />
 
-					<p className="input-label">How much interest will you earn per year?</p>
-					<SliderInput defaultValue={4}/>
-				</div>
-				<div className="financial-display">
-					{/*We have included some sample data here, you will need to replace this
+          <p className="input-label">
+            When are the interest rate calculated?
+          </p>
+          <InterestCalculationType
+            value={this.props.interestCalculationType || "Annually"}
+            onSet={value => setField({ interestCalculationType: value })}
+          />
+
+          <p className="input-label">
+            How much interest will you earn per year?
+          </p>
+          <SliderInput
+            value={this.props.interestRate ? +this.props.interestRate : 4}
+            onSet={value => setField({ interestRate: value })}
+          />
+
+        </div>
+        {this.props.error && <div>Failed to get savings data</div>}
+        <div className="financial-display">
+          {/*We have included some sample data here, you will need to replace this
 					with your own. Feel free to change the data structure if you wish.*/}
-					<DisplayGraph data={[
-						{
-							month: 1,
-							amount:500
-						},
-						{
-							month: 2,
-							amount:700
-						},
-						{
-							month: 3,
-							amount:1000
-						},
-						{
-							month: 4,
-							amount:1500
-						}
-					]}/>
-				</div>
+          {this.props.isFetching && <div>Loading...</div>}
+          <DisplayGraph data={savingsData} />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    initialSavings: state.initialSavings,
+    monthlySavings: state.monthlySavings,
+    interestRate: state.interestRate,
+    interestCalculationType: state.interestCalculationType,
+    savings: state.savings,
+    isFetching: state.isFetching
+  }),
+  { fetchSavings, setField }
+)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { createEpicMiddleware } from "redux-observable";
+import { epic } from "./actions";
+import Reducer from "./reducer";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function logger({ getState }) {
+  return next => action => {
+    console.log("will dispatch", action);
+    let returnValue = next(action);
+    console.log("state after dispatch", getState());
+    return returnValue;
+  };
+}
+
+const epicMiddleware = createEpicMiddleware(epic);
+
+let store = createStore(
+  Reducer,
+  applyMiddleware(epicMiddleware),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById("root")
+);
